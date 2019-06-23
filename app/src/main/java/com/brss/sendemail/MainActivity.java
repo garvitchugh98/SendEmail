4e81dfa1096643c26992bbc98247c0e2f995c246
Garvit Chugh <garvitchugh98@gmail.com>
2019-06-23 07:55:43 +0530

    Updated

--- app/src/main/java/com/brss/sendemail/MainActivity.java
+++ app/src/main/java/com/brss/sendemail/MainActivity.java
@@ -14,47 +14,47 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
 
-//     private EditText emailid, content;
-//     private String email, body;
+    private EditText emailid, content;
+    private String email, body;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//         emailid =  findViewById(R.id.login_emailid);
-//         content =  findViewById(R.id.Content);
+        emailid =  findViewById(R.id.login_emailid);
+        content =  findViewById(R.id.Content);
 
 
-//     Button startBtn = findViewById(R.id.sendBtn);
-//       startBtn.setOnClickListener(new View.OnClickListener()
-//     {
-//         public void onClick (View view){
-//             email = emailid.getText().toString();
-//             body = content.getText().toString();
-//         sendEmail(email,body);
-//     }
-//     });
+    Button startBtn = findViewById(R.id.sendBtn);
+      startBtn.setOnClickListener(new View.OnClickListener()
+    {
+        public void onClick (View view){
+            email = emailid.getText().toString();
+            body = content.getText().toString();
+        sendEmail(email,body);
+    }
+    });
 }
-//     protected void sendEmail(String email, String content) {
-//         Log.i("Send email", "");
-//         String[] TO = {""+email};
-//         String[] CC = {""};
-//         Intent emailIntent = new Intent(Intent.ACTION_SEND);
+    protected void sendEmail(String email, String content) {
+        Log.i("Send email", "");
+        String[] TO = {""+email};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
 
-//         emailIntent.setData(Uri.parse("mailto:"));
-//         emailIntent.setType("text/plain");
-//         emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
-//         emailIntent.putExtra(Intent.EXTRA_CC, CC);
-//         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
-//         emailIntent.putExtra(Intent.EXTRA_TEXT, content);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, content);
 
-//         try {
-//             startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-//             finish();
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
 
-//         } catch (android.content.ActivityNotFoundException ex) {
-//             Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
-//         }
-//     }
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
