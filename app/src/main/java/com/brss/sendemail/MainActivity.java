e0c1e2ca588e228c92a76710ed86df0b94f1998d
garvit <garvitchugh98@gmail.com>
2019-06-23 07:42:24 +0530

    bugs removed

--- app/src/main/java/com/brss/sendemail/MainActivity.java
+++ app/src/main/java/com/brss/sendemail/MainActivity.java
@@ -6,29 +6,39 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText emailid, content;
+    private String email, body;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    Button startBtn = (Button) findViewById(R.id.sendBtn);
+        emailid =  findViewById(R.id.login_emailid);
+        content =  findViewById(R.id.Content);
+
+
+    Button startBtn = findViewById(R.id.sendBtn);
       startBtn.setOnClickListener(new View.OnClickListener()
     {
         public void onClick (View view){
-        sendEmail();
+            email = emailid.getText().toString();
+            body = content.getText().toString();
+        sendEmail(email,body);
     }
     });
 }
-    protected void sendEmail() {
+    protected void sendEmail(String email, String content) {
         Log.i("Send email", "");
-        String[] TO = {""};
+        String[] TO = {""+email};
         String[] CC = {""};
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
 
@@ -37,12 +47,12 @@ public class MainActivity extends AppCompatActivity {
         emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
         emailIntent.putExtra(Intent.EXTRA_CC, CC);
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
-        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, content);
 
         try {
             startActivity(Intent.createChooser(emailIntent, "Send mail..."));
             finish();
-            Log.i("Finished sending email...", "");
+
         } catch (android.content.ActivityNotFoundException ex) {
             Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
         }
